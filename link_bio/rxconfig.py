45d8c3c6ecd16d70e44a2faabc00279a4357cf1c
pparagon23 <165309756+pparagon23@users.noreply.github.com>
2024-05-06 17:01:12 -0600

    Update rxconfig.py
    nueva ip backend vercel

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -5,6 +5,7 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     #api_url="https://python-web-4a2y.onrender.com", #usar con pparagon23 a si esta en github
+    api_url="https://python-web-ochre.vercel.app"
     cors_allowed_origins=[
                          "http://localhost:3000",
                          "http://192.168.100.39:3000", 
