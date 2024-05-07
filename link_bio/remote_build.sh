34a35ac10191c2784b0cafde07aae60479a5824d
pparagon23 <165309756+pparagon23@users.noreply.github.com>
2024-05-06 19:16:15 -0600

    ambiente
    nuevo ambiente

--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -1,8 +1,10 @@
 python -m venv .venv
-source .venv/bin/activate
+source  .venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 rm -rf public
 reflex init
-API_URL="https://python-web-4a2y.onrender.com/" reflex export --frontend-only
+API_URL="https://python-web-4a2y.onrender.com" reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -5,14 +5,11 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     api_url="https://python-web-4a2y.onrender.com", #usar con pparagon23 a si esta en github
-    #api_url="https://python-web-ochre.vercel.app"
     cors_allowed_origins=[
                          "http://localhost:3000",
                          "http://192.168.100.39:3000", 
                          "https://python-web-4a2y.onrender.com"
                          "https://pparagondev.reflex.run"
-                         #"https://pparagondev.reflex.run"
-                         "https://python-7790fdu93-pparagon23s-projects.vercel.app"
                          ]
     
 )
