8fbc88a48b28b79c627df83846c4e1ddf67c6a48
pparagon23 <165309756+pparagon23@users.noreply.github.com>
2024-05-06 18:02:49 -0600

    Update remote_build.sh
    prueba 3

--- link_bio/remote_build.sh
+++ link_bio/remote_build.sh
@@ -1,8 +1,7 @@
-cd link_bio
 python -m venv .venv
 source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 rm -rf public
 reflex init
 API_URL="https://python-web-4a2y.onrender.com/" reflex export --frontend-only
