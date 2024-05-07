bcdad992754a6e2dea8386d280a9214ca712af77
pparagon23 <165309756+pparagon23@users.noreply.github.com>
2024-05-07 01:02:02 -0600

    ambiente 6
    actualiza 6

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -4,16 +4,16 @@ import reflex as rx
 ## cors_allowed limita las peticiones y conexiones del back end linea 7
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://python-web-4a2y.onrender.com", #usar con pparagon23 a si esta en github
-    cors_allowed_origins=[
-                         "http://localhost:3000",
-                         "http://192.168.100.39:3000", 
-                         "https://python-web-4a2y.onrender.com"
-                         "https://pparagondev.reflex.run"
-                         "https://python-web-ochre.vercel.app"
-                         "https://python-web-pparagon23s-projects.vercel.app"
-                         "https://python-web-git-main-pparagon23s-projects.vercel.app"
-                         "https://python-her7slaaw-pparagon23s-projects.vercel.app/"
+    api_url="https://python-web-4a2y.onrender.com", #back end
+    cors_allowed_origins=[ #Front End
+                         "http://localhost:3000"
+                        # "http://192.168.100.39:3000", 
+                        # "https://python-web-4a2y.onrender.com"
+                        # "https://pparagondev.reflex.run"
+                        # "https://python-web-ochre.vercel.app"
+                        # "https://python-web-pparagon23s-projects.vercel.app"
+                        # "https://python-web-git-main-pparagon23s-projects.vercel.app"
+                       #  "https://python-her7slaaw-pparagon23s-projects.vercel.app/"
                          ]
     
 )
