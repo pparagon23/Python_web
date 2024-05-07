0e9da553862471ca6445adaa0f2ab1bd611ae627
pparagon23 <165309756+pparagon23@users.noreply.github.com>
2024-05-07 01:09:09 -0600

    ambiente 6
    actualiza 6

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -4,7 +4,7 @@ import reflex as rx
 ## cors_allowed limita las peticiones y conexiones del back end linea 7
 config = rx.Config(
     app_name="link_bio",
-    api_url="https://python-web-4a2y.onrender.com", #back end
+   # api_url="https://python-web-4a2y.onrender.com", #back end
     cors_allowed_origins=[ #Front End
                          "http://localhost:3000"
                         # "http://192.168.100.39:3000", 
